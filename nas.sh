1e3070982702d9ecf32b6529c865f0f0c1887ad3
kim yongbin <kybinz@gmail.com>
2021-06-24 22:39:49 +0900

    add guide about 'mount -a' to nas.sh

--- nas.sh
+++ nas.sh
@@ -5,4 +5,4 @@ echo "username=" >> /etc/.drnss_credentials
 echo "password=" >> /etc/.drnss_credentials
 chmod 600 /etc/.drnss_credentials
 echo "//nas.kzmdstu.com/drnss /mnt/drnss cifs defaults,uid=1000,gid=1000,credentials=/etc/.drnss_credentials" >> /etc/fstab
-echo "please add credentials to /etc/.drnss_credentials to mount drnss"
+echo "please add credentials to /etc/.drnss_credentials to mount drnss, then mount it with 'mount -a'"
