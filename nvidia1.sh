e15923dac61536b536716d97caff50bb43c94e91
kim yongbin <kybinz@gmail.com>
2021-07-11 01:21:57 +0000

    split nvidia.sh into two sh script

--- nvidia1.sh
+++ nvidia1.sh
@@ -4,5 +4,4 @@ wget -N https://us.download.nvidia.com/XFree86/Linux-x86_64/460.84/NVIDIA-Linux-
 dnf groupinstall -y "Development Tools"
 dnf install -y libglvnd-devel elfutils-libelf-devel
 grub2-editenv - set "$(grub2-editenv - list | grep kernelopts) nouveau.modeset=0"
-reboot
-/mnt/drnss/library/driver/nvidia/NVIDIA-Linux-x86_64-460.84.run
+echo "please reboot and run nvidia2.sh"
---
+++ nvidia2.sh
@@ -0,0 +1,4 @@
+set -e
+
+/mnt/drnss/library/program/driver/nvidia/NVIDIA-Linux-x86_64-460.84.run
+echo "please reboot to nvidia driver takes effect"
