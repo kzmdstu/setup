60af4e31e77435ab05ac8c093454f5ddbb470665
kim yongbin <kybinz@gmail.com>
2021-11-24 11:32:21 +0000

    programatically get the first ethernet network interface

--- idm.sh
+++ idm.sh
@@ -4,8 +4,9 @@ set -e
 # Should've set hostname and dns already.
 
 # hostnamectl set-hostname {hostname}
-# nmcli conn modify {network-interface} ipv4.dns  "{dns ip} 1.1.1.1 1.0.0.1"
-# nmcli conn modify {network-interface} ipv4.ignore-auto-dns yes
+# IFACE=$(nmcli -f DEVICE,TYPE device | grep ethernet | head -n1 | awk '{print $1;}')
+# nmcli conn modify $IFACE ipv4.dns  "{dns ip} 1.1.1.1 1.0.0.1"
+# nmcli conn modify $IFACE ipv4.ignore-auto-dns yes
 
 dnf install freeipa-client
 ipa-client-install --enable-dns-update --mkhomedir
