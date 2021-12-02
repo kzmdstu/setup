ee17a23e4823a0168bcb603c921cae3732cf0c82
kim yongbin <kybinz@gmail.com>
2021-12-02 10:38:26 +0000

    use trial and error method in system.sh

--- system.sh
+++ system.sh
@@ -3,18 +3,17 @@ set -e
 
 # Base Image: Rocky Linux 8.5
 
-# Please do update the distro, then reboot first.
-# It would not work properly otherwise.
-UNAME=$(uname -r)
-# dnf -y update
-
 HERE=$(dirname $BASH_SOURCE)
+
+echo "Updating The Distro"
+dnf -y update
 
 echo "Installing Nvidia Driver"
 dnf -y install epel-release
 dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
-# $(uname -r) trick will not work if `dnf update` has made jump on the latest kernel, but you are still using an old one.
-# Please reboot, then.
+# The $(uname -r) trick might not work if `dnf update` has made jump on the latest kernel,
+# but you are still using an old one.
+# Please reboot and run this script again, then.
 dnf -y install kernel-devel-$(uname -r) kernel-headers-$(uname -r)
 dnf -y install nvidia-driver nvidia-settings
 dnf -y install cuda-driver
