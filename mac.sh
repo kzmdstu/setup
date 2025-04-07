cd4eaba1182836ce6e3fac3f8afb171a66fd2e31
kim yongbin <kybinz@gmail.com>
2025-04-07 03:50:00 -0700

    fix mac script

--- mac.sh
+++ mac.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+set -e
+
+# NOTE: run this command with sudo!
+HERE=$(dirname $BASH_SOURCE)
 
 echo "set name of this computer (without domain)"
-echo -n "name: "
+echo "name: "
 read name
 
 fqdn="${name}.in.kzmdstu.com"
 
 scutil --set HostName "${fqdn}"
 
+mkdir -p /etc/ipa
 scp root@idm01:/etc/ipa/ca.crt /etc/ipa/
 
 ssh admin@idm01 ipa host-add --force "${fqdn}"
-ssh admin@idm01 ipa ipa-getkeytab -p host/"${fqdn}" -k "${name}.keytab"
+ssh admin@idm01 ipa-getkeytab -p host/"${fqdn}" -k "${name}.keytab"
 scp admin@idm01:"${name}.keytab" /etc/krb5.keytab
 
 cp $HERE/data/mac_krb5.conf /etc/krb5.conf
